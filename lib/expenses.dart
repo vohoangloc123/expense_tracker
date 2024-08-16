92967552d891d517d1f22368c03ae7ffc8d2deed
vohoangloc123 <you@example.com>
2024-08-16 21:47:42 +0700

    first commit

---
+++ lib/expenses.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text('The chart'),
+          Text('The list of transactions'),
+        ],
+      ),
+    );
+  }
+}
