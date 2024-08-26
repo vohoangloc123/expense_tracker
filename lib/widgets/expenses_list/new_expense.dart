709694134c55d700fa66fc828c3478b61e2f0cc5
vohoangloc123 <you@example.com>
2024-08-26 14:54:49 +0700

    Handled showing a Date Picker

--- lib/widgets/expenses_list/new_expense.dart
+++ lib/widgets/expenses_list/new_expense.dart
@@ -9,23 +9,43 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpenseState extends State<NewExpense> {
   String _enteredTitle = "";
+
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  final _dateController = TextEditingController();
+
   void _saveTitleInput(String inputValue) {
-    _enteredTitle = inputValue;
+    setState(() {
+      _enteredTitle = inputValue;
+    });
+  }
+
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month - 1);
+    showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      _dateController.text = pickedDate.toString();
+    });
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    _dateController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final _titleController = TextEditingController();
-    final _amountController = TextEditingController();
-    final _dateController = TextEditingController();
-    @override
-    void dispose() {
-      _titleController.dispose();
-      _amountController.dispose();
-      _dateController.dispose();
-      super.dispose();
-    }
-
     return Padding(
       padding: const EdgeInsets.all(16),
       child: Column(
@@ -35,14 +55,33 @@ class _NewExpenseState extends State<NewExpense> {
             maxLength: 50,
             decoration: const InputDecoration(labelText: "Title"),
           ),
-          TextField(
-            controller: _amountController,
-            keyboardType: TextInputType.number,
-            decoration:
-                const InputDecoration(prefixText: '\$ ', labelText: "Amount"),
-          ),
-          const TextField(
-            decoration: InputDecoration(labelText: "Date"),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      prefixText: '\$ ', labelText: "Amount"),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment:
+                      MainAxisAlignment.end, // Aligns the children to the right
+                  crossAxisAlignment: CrossAxisAlignment
+                      .center, // Aligns the children to the center
+                  children: [
+                    const Text("Selected Date"),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_today),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
           Row(
             children: [
@@ -56,9 +95,9 @@ class _NewExpenseState extends State<NewExpense> {
                   print(_amountController.text);
                 },
                 child: const Text("Add Expense"),
-              )
+              ),
             ],
-          )
+          ),
         ],
       ),
     );
